fa41fd1ab86e3680378a308d467c480ae21c4dc0
Sangram Ragad <sangram.r121@gmail.com>
2020-05-20 11:13:01 +0530

    Added production setup

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-require("dotenv").config();
-
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
@@ -49,7 +47,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 //PORT
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 
 //Starting a server
 app.listen(port, () => {
