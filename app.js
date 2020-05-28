71b578cc4420d31e9ef321caee6afc437b2ca4c8
Sangram Ragad <sangram.r121@gmail.com>
2020-05-28 16:25:31 +0530

    updating code

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use("/api", orderRoutes);
 app.use("/api", paymentBRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
